9a87acf4da037219aaa2584fe04b5fedb4aa065b
Silvanei Martins <silvaneimartins_rcc@hotmail.com>
2023-01-16 00:23:44 -0300

    Configuração static do backend python

--- main.py
+++ main.py
@@ -1,12 +1,14 @@
 from fastapi import FastAPI
+from fastapi.staticfiles import StaticFiles
 
 from routes import products, users
 
 app = FastAPI()
 
-# Rotas de produtos
+# Rotas da aplicação
 app.include_router(products.router)
 app.include_router(users.router)
+app.mount('/static', StaticFiles(directory='static'), name='static')
 
 @app.get('/')
 async def root():
